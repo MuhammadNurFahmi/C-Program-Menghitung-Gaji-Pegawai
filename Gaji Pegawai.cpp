200e4886a1c29cb16e7d2db1373affae616befd6
MuhammadNurFahmi <45891167+MuhammadNurFahmi@users.noreply.github.com>
2018-12-17 10:01:22 +0700

    Add files via upload

---
+++ Gaji Pegawai.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+int gaji,jumlah;
+
+cout << "menghiitung gaji pegawai jika perjam 5000" <<endl;
+cout << "masukkan jumlah jam kerja : ";
+cin >> jumlah;
+
+gaji = jumlah * 5000;
+cout << "gaji pegawai adalah :" <<gaji;
+
+return 0;	
+}
